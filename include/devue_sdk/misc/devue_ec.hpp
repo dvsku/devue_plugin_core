77075b56efbdd1c89f8db79a02a62f86a7a9d2af
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-09-23 17:11:10 +0200

    Add bool operator to devue_ec

--- include/devue_sdk/misc/devue_ec.hpp
+++ include/devue_sdk/misc/devue_ec.hpp
@@ -6,5 +6,9 @@ namespace devue::sdk {
     struct devue_ec {
         int64_t     code    = 0;
         std::string message = "";
+
+        explicit operator bool() {
+            return code != 0;
+        }
     };
 }
