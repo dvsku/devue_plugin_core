ac3f30cad8ec2de130d6880e4e2f70e243fc10e9
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-09-23 02:02:57 +0200

    Update defines

--- include/devue_sdk/defines.hpp
+++ include/devue_sdk/defines.hpp
@@ -6,20 +6,13 @@
     #define DEVUE_API extern "C" __declspec(dllexport)
 #endif
 
-constexpr uint16_t SDK_VERSION_YEAR  = 2024;
-constexpr uint16_t SDK_VERSION_MONTH = 9;
-constexpr uint16_t SDK_VERSION_DAY   = 23;
-constexpr uint16_t SDK_VERSION_REV   = 0;
+constexpr uint16_t DEVUE_SDK_VERSION_YEAR  = 2024;
+constexpr uint16_t DEVUE_SDK_VERSION_MONTH = 9;
+constexpr uint16_t DEVUE_SDK_VERSION_DAY   = 23;
+constexpr uint16_t DEVUE_SDK_VERSION_REV   = 0;
 
-#define SDK_VERSION_PACKED                  \
-    ((uint64_t)SDK_VERSION_YEAR  << 48) |   \
-    ((uint64_t)SDK_VERSION_MONTH << 32) |   \
-    ((uint64_t)SDK_VERSION_DAY   << 16) |   \
-    ((uint64_t)SDK_VERSION_REV)
-    
-
-//#define SDK_VERSION_PACKED                  \
-//    ((uint64_t)SDK_VERSION_YEAR  << 48) |   \
-//    (((uint64_t)SDK_VERSION_MONTH << 32)) |   \ 
-//    (((uint64_t)SDK_VERSION_DAY   << 16)) |   \
-//    (((uint64_t)SDK_VERSION_REV))
+#define DEVUE_SDK_VERSION_PACKED                  \
+    ((uint64_t)DEVUE_SDK_VERSION_YEAR  << 48) |   \
+    ((uint64_t)DEVUE_SDK_VERSION_MONTH << 32) |   \
+    ((uint64_t)DEVUE_SDK_VERSION_DAY   << 16) |   \
+    ((uint64_t)DEVUE_SDK_VERSION_REV)
