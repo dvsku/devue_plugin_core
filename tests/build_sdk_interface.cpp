729a5ffa3a9cf631aec5f02a50222100d21a30dd
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-09-24 03:55:35 +0200

    Update tests

--- tests/build_sdk_interface.cpp
+++
@@ -1,9 +0,0 @@
-#include <devue_sdk.hpp>
-
-int main() {
-    devue::sdk::devue_model_pack        models    = {};
-    devue::sdk::devue_image             image     = {};
-    devue::sdk::devue_plugin_interface* interface = nullptr;
-
-    return 0;
-}
